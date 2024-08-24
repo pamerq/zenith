66bcf6c99328325f548c09f83230ae84f5feb9e7
pamerq <pame@Pames-MacBook-Pro.local>
2024-08-24 17:19:54 -0500

    Add authMiddleware

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const TaskController = require('../controllers/taskController');
-//const authenticateToken = require('../middlewares/authMiddleware');
+const authenticateToken = require('../middlewares/authMiddleware');
 
 // Create a new task
-router.post('/', /*authenticateToken, */ TaskController.createTask);
+router.post('/', authenticateToken, TaskController.createTask);
 
 // Get all tasks
-router.get('/', /*authenticateToken, */ TaskController.getTasks);
+router.get('/', authenticateToken, TaskController.getTasks);
 
 // Get a single task by ID
-router.get('/:id', /*authenticateToken, */ TaskController.getTask);
+router.get('/:id', authenticateToken, TaskController.getTask);
 
 // Update a task by ID
-router.put('/:id', /*authenticateToken, */ TaskController.updateTask);
+router.put('/:id', authenticateToken, TaskController.updateTask);
 
 // Delete a task by ID
-router.delete('/:id', /*authenticateToken, */ TaskController.deleteTask);
+router.delete('/:id', authenticateToken, TaskController.deleteTask);
 
 module.exports = router;
