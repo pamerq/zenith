b3a0f01a7fdae04b652637bef52c9464ad7fc66a
pamerq <pame@Pames-MacBook-Pro.local>
2024-10-26 23:07:53 -0500

    Delete menu of Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import Tasks from './components/Tasks';
 import Login from './components/Login';
 import Welcome from './components/Welcome';
@@ -8,19 +8,30 @@ import './styles/style.scss';
 const App = () => {
   return (
     <Router>
-      <div className="container">
+      <Navigation />
+      <Routes>
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const Navigation = () => {
+  const location = useLocation();
+  const hideMenuRoutes = ['/login'];
+
+  return (
+    <div className="container">
+      {!hideMenuRoutes.includes(location.pathname) && (
         <nav>
-          <Link to="/">Home</Link><br></br>
-          <Link to="/tasks">Tasks</Link><br></br>
+          <Link to="/">Home</Link><br />
+          <Link to="/tasks">Tasks</Link><br />
           <Link to="/login">Login</Link>
         </nav>
-        <Routes>
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/welcome" element={<Welcome />} />
-        </Routes>
-      </div>
-    </Router>
+      )}
+    </div>
   );
 };
 
